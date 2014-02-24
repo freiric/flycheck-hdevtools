22caad717a7b42f0d97210c25d3f9a15c52222e1
Freiric Barral <freiric@gmail.com>
2014-02-24 22:19:06 +0100

    options to be passed to hdevtools can be packed  in a ``.ghc-options`` file.

--- flycheck-hdevtools.el
+++ flycheck-hdevtools.el
@@ -34,12 +34,62 @@
 ;;; Code:
 
 (require 'flycheck)
+(require 'haskell-cabal)
+
+;;;###autoload
+(defun flycheck-hdevtools-setup ()
+  "Setup Cabal/GHC integration for Flycheck.
+
+If the current file is part of a Cabal project and there is a .ghc-options file,
+as denoted by the existence of a Cabal and .ghc-options file in the file's
+directory or any ancestor thereof, change to cabal directory and pass the
+options in .ghc-options to hdevtools.
+
+Set `flycheck-haskell-options-finder' accordingly."
+  (setq flycheck-haskell-options-finder
+	(eval '(ghc-options-finder)))
+)
+
+(defun ghc-options-finder ()
+  "Change to cabal directory and return the options list found in .ghc-options.
+
+Find a Cabal and a .ghc-options file in some superdirectory, change to the
+directory of the Cabal file and read the ghc options into a list.
+Returns this options list."
+(when (buffer-file-name)
+    (-when-let* ((file (haskell-cabal-find-file))
+		 (cabal-dir (when file (file-name-directory file)))
+		 (root-dir (locate-dominating-file (buffer-file-name) ".ghc-options"))
+		 (ghc-options-file (concat root-dir ".ghc-options"))
+		 (options-buffer (find-file-noselect ghc-options-file))
+		 (ghc-options (with-current-buffer options-buffer
+				(split-string (buffer-string))))
+                 ;; (hdevtools-options
+                 ;;  (mapcar (lambda (s) (concat "-g" s))  ghc-options))
+		 )
+      (cd cabal-dir)
+      ;; (message "ghc options found: %S" ghc-options)
+      ;; (message "hdevtools options found: %S" hdevtools-options)
+      ghc-options
+      )))
+
+
+(flycheck-def-option-var flycheck-haskell-options-finder nil haskell-hdevtools
+  "Function retrieving ghc options to use for hdevtools.
+
+The value of this option is a function returning list of options to be passed to hdevtools."
+  :type '(function :tag "haskell options")
+  :safe #'stringp
+  :package-version '(flycheck . "0.15"))
+
 
 (flycheck-define-checker haskell-hdevtools
   "A Haskell syntax and type checker using hdevtools.
 
 See URL `https://github.com/bitc/hdevtools'."
-  :command ("hdevtools" "check" "-g" "-Wall" source-inplace)
+  :command ("hdevtools" "check"
+            (eval (flycheck-prepend-with-option "-g" flycheck-haskell-options-finder))
+	    "-g" "-Wall" source-inplace)
   :error-patterns
   ((warning line-start (file-name) ":" line ":" column ":"
             (or " " "\n    ") "Warning:" (optional "\n")
